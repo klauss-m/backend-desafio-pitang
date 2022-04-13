a885232122d2dae946bf22887b7d5efa0bb65d9d
Klauss Marques <klauss.marques@gmail.com>
2022-04-13 17:14:39 -0300

    feat: create healthcheck route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
+import { healthcheckRouter } from './routes/healthcheck.routes';
+
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
 server.use(cors());
+server.use('/healthcheck', healthcheckRouter);
 
 export { server };
