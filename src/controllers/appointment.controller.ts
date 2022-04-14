df18ca5e5becbc2264d973bf4b979cb86a0eae64
Klauss Marques <klauss.marques@gmail.com>
2022-04-14 20:24:51 -0300

    feat: add postAppointment controller

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
-import { getAppointments } from '../services/appointment.service';
+import {
+  appointmentValidation,
+  getAppointments,
+  postAppointment,
+} from '../services/appointment.service';
 
 export async function appointmentsList(req: Request, res: Response) {
   try {
@@ -8,5 +12,23 @@ export async function appointmentsList(req: Request, res: Response) {
   } catch (err) {
     const error = err as Error;
     return res.status(500).json({ message: error.message });
+  }
+}
+
+export async function appointmentCreate(req: Request, res: Response) {
+  const validation = await appointmentValidation(req.body);
+  if (validation.length) {
+    return res.status(400).json({
+      errors: validation.map((e) => {
+        const msg = Object.values(e.constraints!);
+        return msg.join(' ');
+      }),
+    });
+  }
+  try {
+    const insert = await postAppointment(req.body);
+    return res.status(201).json(insert);
+  } catch (err) {
+    return res.status(400).json({ message: 'Failed to create an appointment.' });
   }
 }
