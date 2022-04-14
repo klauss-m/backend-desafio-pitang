70628e35224fab4d7d3268c23bc1accb2c5af8fe
Klauss Marques <klauss.marques@gmail.com>
2022-04-14 13:32:02 -0300

    feat: add postAppointment service

--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,3 +1,4 @@
+import { Appointment } from '@prisma/client';
 import { DateTime } from 'luxon';
 import { prisma } from '../db/prisma';
 
@@ -9,4 +10,13 @@ export async function getAppointments() {
     appointmentDate: DateTime.fromJSDate(reg.appointmentDate).toFormat('dd/LL/yyyy TT'),
   }));
   return appointments;
+}
+
+export async function postAppointment(data: Appointment) {
+  const input = {} as any;
+  input.name = data.name;
+  input.dateOfBirth = new Date(data.dateOfBirth);
+  input.appointmentDate = new Date(data.appointmentDate);
+  const registry = await prisma.appointment.create({ data: input });
+  return registry;
 }
