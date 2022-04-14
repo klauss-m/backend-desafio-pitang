c0ff7a60ecb711f506b873b917e901736b7dd1c3
Klauss Marques <klauss.marques@gmail.com>
2022-04-14 20:13:30 -0300

    feat: add appointment validation using class-validator

--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,6 +1,20 @@
 import { Appointment } from '@prisma/client';
 import { DateTime } from 'luxon';
+import { validate, IsAlpha, IsDateString, IsString, Length } from 'class-validator';
 import { prisma } from '../db/prisma';
+
+class AppointmentValidator {
+  @IsString()
+  @IsAlpha('pt-BR', { message: 'The name must contain alphanumeric characters only.' })
+  @Length(3, 50, { message: 'The name must contain between 3 and 50 characteres only.' })
+  name!: string;
+
+  @IsDateString({ message: 'Must contain a valid date.' })
+  dateOfBirth!: string;
+
+  @IsDateString({ message: 'Must contain a valid date.' })
+  appointmentDate!: string;
+}
 
 export async function getAppointments() {
   const registries = await prisma.appointment.findMany();
@@ -19,4 +33,14 @@ export async function postAppointment(data: Appointment) {
   input.appointmentDate = new Date(data.appointmentDate);
   const registry = await prisma.appointment.create({ data: input });
   return registry;
+}
+
+export async function appointmentValidation(data: Appointment) {
+  const val = new AppointmentValidator();
+  val.name = data.name;
+  val.dateOfBirth = data.dateOfBirth.toString();
+  val.appointmentDate = data.appointmentDate.toString();
+
+  const validation = await validate(val);
+  return validation;
 }
