f2d00158ef515da8a26c28535e6a420ecb927107
Klauss Marques <klauss.marques@gmail.com>
2022-04-24 20:18:43 -0300

    fix: server port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { server } from './server';
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // https://stackoverflow.com/a/62447944/17163926
